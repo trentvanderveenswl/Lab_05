4eb4baf2ca2b86a8940b0ddcfc63aad5f0db5ae9
Trent <vanderveen.615726@swl.k12.oh.us>
2025-09-23 08:15:29 -0400

    Finshed Task4.

---
+++ src/Task4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int age;
+
+        if (scan.hasNextInt()) {
+            age = scan.nextInt();
+            if (age >= 21) {
+                System.out.println("You get a paper wristband.");
+            }
+        } else {
+            System.out.println("You did not enter a valid age.");
+        }
+    }
+}
